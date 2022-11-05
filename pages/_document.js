439bb10e60106107e4632ccc65e7dd909c629ded
Bruno Lepis <brunka215@gmail.com>
2022-11-05 14:42:16 +0100

    Add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="title" content="World Cities" />
+        <meta
+          name="description"
+          content="Name all the cities you know in the world."
+        />
+        <meta
+          name="keywords"
+          content="map, world, openstreetmap, city, mapbox, cities, guess"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+      </Head>
       <body className="bg-[#191a1a]">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
